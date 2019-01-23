87ca6154f5f8688f025595b4320c95e22922f91d
Jeremy Gerson <jgerson@hashicorp.com>
2019-01-22 21:29:19 -0500

    Update main.tf

--- aws-ApplicationBase/main.tf
+++ aws-ApplicationBase/main.tf
@@ -14,29 +14,6 @@ data "aws_route53_zone" "hashidemos" {
   name = "hashidemos.io."
 }
 
-module "ec2" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "1.3.0"
-
-  instance_count              = 2
-  name                        = "${var.application_name}-demo"
-  ami                         = "${var.ami_id}"
-  instance_type               = "${var.instance_type}"
-  subnet_id                   = "${element(data.terraform_remote_state.networkdetails.public_subnets, 0)}"
-  vpc_security_group_ids      = ["${data.terraform_remote_state.networkdetails.security_group}"]
-  associate_public_ip_address = true
-  key_name                    = "${var.key_name}"
-
-  root_block_device = [{
-    volume_size = "50"
-  }]
-
-  tags = {
-    Owner = "${var.owner}"
-    TTL   = "${var.ttl}"
-  }
-}
-
 locals {
   namespace = "${var.namespace}-demo"
 }
